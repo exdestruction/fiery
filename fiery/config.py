b9d53ba18f3737d53368c55f9c053d5d40389a04
Semyon Kondratev <execute.ink@gmail.com>
2022-01-05 20:03:41 +0100

    Done with __get_data_timestamp(). Made crutch in fiery/config.py

--- fiery/config.py
+++ fiery/config.py
@@ -3,28 +3,28 @@ from fvcore.common.config import CfgNode as _CfgNode
 
 
 def convert_to_dict(cfg_node, key_list=[]):
-    """Convert a config node to dictionary."""
-    _VALID_TYPES = {tuple, list, str, int, float, bool}
-    if not isinstance(cfg_node, _CfgNode):
-        if type(cfg_node) not in _VALID_TYPES:
-            print(
-                'Key {} with value {} is not a valid type; valid types: {}'.format(
-                    '.'.join(key_list), type(cfg_node), _VALID_TYPES
-                ),
-            )
-        return cfg_node
-    else:
-        cfg_dict = dict(cfg_node)
-        for k, v in cfg_dict.items():
-            cfg_dict[k] = convert_to_dict(v, key_list + [k])
-        return cfg_dict
+	"""Convert a config node to dictionary."""
+	_VALID_TYPES = {tuple, list, str, int, float, bool}
+	if not isinstance(cfg_node, _CfgNode):
+		if type(cfg_node) not in _VALID_TYPES:
+			print(
+				'Key {} with value {} is not a valid type; valid types: {}'.format(
+					'.'.join(key_list), type(cfg_node), _VALID_TYPES
+				),
+			)
+		return cfg_node
+	else:
+		cfg_dict = dict(cfg_node)
+		for k, v in cfg_dict.items():
+			cfg_dict[k] = convert_to_dict(v, key_list + [k])
+		return cfg_dict
 
 
 class CfgNode(_CfgNode):
-    """Remove once https://github.com/rbgirshick/yacs/issues/19 is merged."""
+	"""Remove once https://github.com/rbgirshick/yacs/issues/19 is merged."""
 
-    def convert_to_dict(self):
-        return convert_to_dict(self)
+	def convert_to_dict(self):
+		return convert_to_dict(self)
 
 
 CN = CfgNode
@@ -127,26 +127,29 @@ _C.GRAD_NORM_CLIP = 5
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='Fiery training')
-    # TODO: remove below?
-    parser.add_argument('--config-file', default='', metavar='FILE', help='path to config file')
-    parser.add_argument(
-        'opts', help='Modify config options using the command-line', default=None, nargs=argparse.REMAINDER,
-    )
-    return parser
+	parser = argparse.ArgumentParser(description='Fiery training')
+	# TODO: remove below?
+	parser.add_argument('--config-file', default='', metavar='FILE', help='path to config file')
+	parser.add_argument(
+		'opts', help='Modify config options using the command-line', default=None, nargs=argparse.REMAINDER,
+	)
+	return parser
 
 
 def get_cfg(args=None, cfg_dict=None):
-    """ First get default config. Then merge cfg_dict. Then merge according to args. """
+	""" First get default config. Then merge cfg_dict. Then merge according to args. """
 
-    cfg = _C.clone()
+	cfg = _C.clone()
 
-    if cfg_dict is not None:
-        cfg.merge_from_other_cfg(CfgNode(cfg_dict))
+	if cfg_dict is not None:
+		cfg.merge_from_other_cfg(CfgNode(cfg_dict))
 
-    if args is not None:
-        if args.config_file:
-            cfg.merge_from_file(args.config_file)
-        cfg.merge_from_list(args.opts)
-        cfg.freeze()
-    return cfg
+	if args is not None:
+		if args.config_file:
+			cfg.merge_from_file(args.config_file)
+		try:
+			cfg.merge_from_list(args.opts)
+		except AttributeError:
+			pass
+		cfg.freeze()
+	return cfg
